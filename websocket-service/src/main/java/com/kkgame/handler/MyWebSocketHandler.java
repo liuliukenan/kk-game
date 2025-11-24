cc5cea6c6b5051dc0cc0e00ad110e1d177dd06bd
lkn <812091690@qq.com>
2025-11-24 11:30:43 +0800

    kk-game 完成ws消息接收,并实现自定义路由负载均衡到对应的服务

--- websocket-service/src/main/java/com/kkgame/handler/MyWebSocketHandler.java
+++ websocket-service/src/main/java/com/kkgame/handler/MyWebSocketHandler.java
@@ -1,32 +1,41 @@
 package com.kkgame.handler;
 
 import com.kkgame.service.WebSocketService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import javax.annotation.Resource;
+
 @Component
+@Slf4j
 public class MyWebSocketHandler extends TextWebSocketHandler {
 
     // 注入WebSocket服务
-    @Autowired
+    @Resource
     private WebSocketService webSocketService;
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
+        log.debug("New WebSocket connection established - Session ID: {}", session.getId());
         webSocketService.handleConnectionEstablished(session);
     }
 
     @Override
     protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        log.debug("Received binary messageSession ID: {}, Close Status: {}",
+                session.getId(), message.getPayloadLength());
         webSocketService.handleBinaryMessage(session, message);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        // 添加日志记录，使用 session 和 status 参数
+        log.debug("WebSocket connection closed - Session ID: {}, Close Status: {}",
+                session.getId(), status.getCode());
         webSocketService.handleConnectionClosed(session, status);
     }
 }
